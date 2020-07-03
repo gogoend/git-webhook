e27c854ab8a39cdbe7ed2e9d9b2ba0db2146bb46
gogoend <gogoend@qq.com>
2020-07-04 01:35:34 +0800

    fix: 调整命令，使用shell_exec

--- build-cli.php
+++ build-cli.php
@@ -3,7 +3,7 @@
  * @Author: gogoend
  * @Date: 2020-07-04 00:05:17
  * @LastEditors: gogoend
- * @LastEditTime: 2020-07-04 01:26:22
+ * @LastEditTime: 2020-07-04 01:34:54
  * @FilePath: \git-webhook\build-cli.php
  * @Description: 
  */
@@ -32,4 +32,4 @@ if (!file_exists($runBuildFilePath . '/runbuild.sh')) {
     exit(1);
 }
 
-echo (exec($runBuildFilePath. ' 2>&1'));
+echo (shell_exec("cd $runBuildFilePath && ./runbuild.sh 2>&1"));
