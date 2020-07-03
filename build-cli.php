6d487849b89d855ffda5e0eb18da12661668b0dd
gogoend <gogoend@qq.com>
2020-07-04 00:32:10 +0800

    enhance: 判断执行中可能发生的错误

--- build-cli.php
+++ build-cli.php
@@ -3,23 +3,29 @@
  * @Author: gogoend
  * @Date: 2020-07-04 00:05:17
  * @LastEditors: gogoend
- * @LastEditTime: 2020-07-04 00:13:19
+ * @LastEditTime: 2020-07-04 00:32:08
  * @FilePath: \git-webhook\build-cli.php
  * @Description: 
- */ 
+ */
 
- var_dump($argv);
-
- $repoName = $argv[1];
- if (file_exists('auth-config.php')) {
+// var_dump($argv);
+if (array_key_exists(1, $repoName)) {
+    $repoName = $argv[1];
+} else {
+    echo '发生错误 - 参数中未指定仓库名称';
+    exit(1);
+}
+if (file_exists('auth-config.php')) {
     include('auth-config.php');
 } else {
     echo '发生错误 - 配置文件无效';
     exit(1);
 }
- if(!$argv[1]){
-     echo '未指定仓库';
-     exit(1);
- }
 
- echo (exec(PROJECT_BASE_FOLDER.$repoName.'/runbuild.sh'));
+$runBuildFilePath = PROJECT_BASE_FOLDER . $repoName . '/runbuild.sh';
+if (!file_exists($runBuildFilePath)) {
+    echo "发生错误 - 找不到 $repoName 仓库根目录下的runbuild.sh";
+    exit(1);
+}
+
+echo (exec($runBuildFilePath . ' 2>&1'));
